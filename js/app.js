3da1b084f5aed3ad4be66432fe1586a1078b9d84
John Giglio <JGiglio@ucvts.tec.nj.us>
2019-12-13 10:52:16 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    document.getElementById("add").onclick = addItem;
+}
+
+const list = [];
+
+const addItem = function(); {
+  let input = document.getElementById("input").value;
+  console.log(input);
+}
