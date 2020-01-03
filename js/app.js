d10430276702d030a54f5080d6d4d558303fb5a1
j-giglio <55398564+j-giglio@users.noreply.github.com>
2020-01-03 09:05:01 -0500

    made a couple changes

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ window.onload = function() {
     document.getElementById("add").onclick = addItem;
 }
 
-let list = [];
+let listOfItems = [];
 let toDo = document.getElementById("toDo");
+let itemNumber = 0;
 
 const addItem = function() {
   let input = document.getElementById("textbox").value;
   const listItem = document.createElement("li");
+  String(itemNumber);
+  listItem.setAttribute(id = "item" + itemNumber);
+  itemNumber++;
+  
   listItem.innerHTML = input;
   toDo.append(listItem);
 }
