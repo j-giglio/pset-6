36437ccd9541327745620b0baef0939c3103dacb
j-giglio <55398564+j-giglio@users.noreply.github.com>
2020-01-04 09:46:23 -0500

    removed debugging logs and added missing bracket

--- js/app.js
+++ js/app.js
@@ -7,17 +7,15 @@ let toDo = document.getElementById("toDo");
 
 function addItem() {
   let input = document.getElementById("textbox").value;
-  console.log(input)
   const listItem = document.createElement("li");
   listItem.id = "item"
   listItem.innerHTML = input;
-  console.log(listItem)
   listOfItems.push(listItem);
-  console.log(listOfItems)
   displayItems
 }
 
 function displayItems() {
   for (let i = 0; i < listOfItems.length; i++) {
     toDo.append(listOfItems[i]);
+  }
 }
