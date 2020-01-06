3a85f8aab3abaee700ad3344e802d6e944ea29fe
j-giglio <55398564+j-giglio@users.noreply.github.com>
2020-01-05 23:35:47 -0500

    code works in theory, find how to get index

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     document.getElementById("add").onclick = addItem;
+    document.getElementByClassName(priorityButton).onclick = raisePriority;
+    document.getElementByClassName(doneButton).onclick = markDone;
+    document.getElementByClassName(xButton).onclick = removeItem;
 }
 
 let listOfItems = [];
@@ -7,28 +10,57 @@ let toDo = document.getElementById("toDo");
 
 function addItem() {
   let input = document.getElementById("textbox").value;
-  const listItem = document.createElement("span");
+  const listItem = document.createElement("p");
   listItem.id = "item";
-  const exclamation = document.createElement("p");
-  const check = document.createElement("p");
-  const listText = document.createElement("p");
-  exclamation.innerHTML = "!";
-  check.innerHTML = "✓";
-  exclamation.class = "priorityButton";
-  check.class = "doneButton";
+  listItem.className = "incomplete";
+  const exclamation = document.createElement("span");
+  const check = document.createElement("span");
+  const cross = document.createElement("span");
+  const listText = document.createElement("span");
+  exclamation.innerHTML = "! ";
+  check.innerHTML = "✓ ";
+  cross.innerHTML = "✗ ";
+  exclamation.className = "priorityButton";
+  check.className = "doneButton";
+  cross.className = "removeButton";
   listText.innerHTML = input;
   listItem.appendChild(exclamation);
   listItem.appendChild(check);
   listItem.appendChild(listText);
+  listItem.appendChild(cross);
   listOfItems.push(listItem);
-  console.log(listOfItems)
+  console.log(listOfItems);
+  exclamationArray = document.getElementByClassName(priorityButton);
+  checkArray = document.getElementByClassName(doneButton);
   displayItems();
 }
 
 function displayItems() {
-//   toDo.innerHTML = ""
+  toDo.innerHTML = "";
   for (let i = 0; i < listOfItems.length; i++) {
     toDo.appendChild(listOfItems[i]);
     console.log(listOfItems[i]);
   }
+}
+
+function raisePriority {
+  const index = ;
+  listOfItems.unshift(listOfItems.splice(index, 1)[0]);
+  displayItems();
+}
+
+function markDone {
+  const index = ;
+  if (index.className == "incomplete") {
+    index.className = "complete";
+  } else if (index.className == "complete") {
+    index.className = "incomplete";
+  }
+  displayItems();
+}
+
+function removeItem {
+  const index = ;
+  listOfItems.push(listOfItems.splice(index, 1)[0]);
+  displayItems();
 }
